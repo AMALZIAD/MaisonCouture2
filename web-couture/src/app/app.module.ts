729d4dd3b58ff0c76c010a7b893e5f48fd1cf2d5
amal ziad <git config --global user.email>
2023-03-18 15:22:47 +0100

    web couture 2

--- web-couture/src/app/app.module.ts
+++ web-couture/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CouturiesComponent } from './couturies/couturies.component';
+import { CouturierComponent } from './couturier/couturier.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    CouturiesComponent,
+    CouturierComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
